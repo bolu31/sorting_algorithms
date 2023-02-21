9e5831da339aad8db87625b7f5fe9cb07fdf61f6
boluwatife <96779419+bolu31@users.noreply.github.com>
2023-02-21 13:59:10 +0100

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,27 +8,22 @@
 void swap_ints(int *a, int *b)
 {
 	int tmp;
-
 	tmp = *a;
 	*a = *b;
 	*b = tmp;
 }
-
 /**
  * bubble_sort - Sort an array of integers in ascending order.
  * @array: An array of integers to sort.
  * @size: The size of the array.
- *
  * Description: Prints the array after each swap.
  */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, len = size;
 	bool bubbly = false;
-
 	if (array == NULL || size < 2)
-		return;
-
+	return;
 	while (bubbly == false)
 	{
 		bubbly = true;
